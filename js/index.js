9d61a633c168604cc8e5d951afc5c8612658d957
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-04-07 17:27:08 -0500

    dont with task2

--- js/index.js
+++ js/index.js
@@ -5,11 +5,14 @@ const get = (selector) => {
 
 //Variables
 const signUp1 = get('body > div > section.content-pick > div:nth-child(1) > div')
-const signUp2 = get('body > div > section.content-pick > div:nth-child(1) > div')
+const signUp2 = get('body > div > section.content-pick > div:nth-child(2) > div')
 const signUp3 = get('body > div > section.content-pick > div:nth-child(3) > div')
 const logoTop = get('body > header > div > h1')
 const welcomeH1 = get('body > div > header > h2')
 const heroImg = get('body > div > header > img')
+const firstP = get('body > div > header > p')
+const footerText = get('body > footer > p')
+const btmPic = get('body > div > section.content-destination > img')
 
 
 
@@ -38,6 +41,41 @@ function removeHero(event){
 }
 heroImg.addEventListener('click', removeHero)
 
+//Keydown
+function escKey(event) {
+    if (event.key === 'Escape') {
+        logoTop.remove()
+    }
+  }
+  document.addEventListener('keydown', escKey)
+
+//Double Click on the 2nd button
+function btn2(event){
+    signUp2.style.color = 'red';
+}
+signUp2.addEventListener('dblclick', btn2)
+
+//Remove 3rd Sign up
+function btn3(event){
+    signUp3.remove()
+}
+signUp3.addEventListener('click', btn3)
+
+//Change background color of first paragraph
+function pFirst(event){
+    firstP.style.backgroundColor = 'red'
+}
+firstP.addEventListener('mouseover', pFirst)
+
+//Change background color footer
+function footerP(event){
+    footerText.style.backgroundColor = 'grey'
+}
+footerText.addEventListener('click', footerP)
 
 
-
+//Scroll
+function pic(event){
+    alert('Do Not Fall In!!!!')
+}
+btmPic.addEventListener('click', pic)
